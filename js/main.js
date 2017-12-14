9c9c622d11f20b8f7fb9d6122e98c0d3fab5dd94
Josue Villasante <javillasantet@gmail.com>
2017-12-14 17:18:36 -0500

    finished all form styles and ticket sales y wip

--- js/main.js
+++ js/main.js
@@ -1,5 +1,34 @@
-$(document).ready(function() {
-    if(! /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-        $('#select-origen, #select-destino').select2();
-    }
-});
+function main() {
+	function calculate() {
+		let precio = $('#inp-precio-venta').val();
+		let ingresos = (precio - 30) / 1.15;
+		let comision = ingresos * 0.15;
+
+		if (ingresos < 0 || comision < 0) {
+			comision = 0;
+			ingresos = 0;
+		}
+
+		$('#inp-com-venta').val(`US$ ${parseResult(comision)}`);
+		$('#inp-ingresos').val(`US$ ${parseResult(ingresos)}`);
+	}
+
+	function parseResult(result) {
+		if (result === 0) return 0;
+		if (result % 1 === 0) {
+			result = `${result}.00`
+		} else {
+			result = round(result);
+		}
+
+		return result;
+	}
+
+	function round(x) {
+		return Number(Math.round(x +'e2')+'e-2');
+	}
+
+	$('#inp-precio-venta').keyup(calculate);
+}
+
+$(main);
