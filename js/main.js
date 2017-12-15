14b46f2200524850094e01bc65d85ebb44218f39
Josue Villasante <javillasantet@gmail.com>
2017-12-15 01:45:25 -0500

    monton de cambios

--- js/main.js
+++ js/main.js
@@ -28,6 +28,24 @@ function main() {
 		return Number(Math.round(x +'e2')+'e-2');
 	}
 
+	function onSelectedTicket() {
+		let box = $(this).closest('.sell-ticket-box');
+		$(box.find('.ticket-selected')).css('background-color', 'rgba(0, 0, 0, 0.75)').css('z-index', 4);
+		$(box.find('.confirm-icon')).css('opacity', '1');
+	}
+
+	function onDeselectedTicket() {
+		let elem = $(this);
+		elem.css('background-color', 'rgba(0, 0, 0, 0)');
+		elem.find('.confirm-icon').css('opacity', 0);
+
+		setTimeout(function() {
+			elem.css('z-index', 0)
+		}, 500);
+	}
+
+	$('.ticket-selected').click(onDeselectedTicket);
+	$('.sell-ticket-box .icon-Add-Bag').click(onSelectedTicket);
 	$('#inp-precio-venta').keyup(calculate);
 }
 
